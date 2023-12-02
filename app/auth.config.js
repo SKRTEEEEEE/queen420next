e4e53b2f207aa6520970777e30d717ed722c6438
Adan <adanreh.m@gmail.com>
2023-12-02 15:12:29 +0100

    Debugged!

--- app/auth.config.js
+++ app/auth.config.js
@@ -10,23 +10,12 @@ export const authConfig = {
       const isOn = nextUrl.pathname.startsWith('/');
       const isOnMain = nextUrl.pathname.startsWith('/main'); //<- /main to /
 
-      /*nextUrl esta vacio
-      El error NEXT_REDIRECT indica que se está intentando realizar una redirección en el servidor, lo cual puede ser problemático ya que ciertas operaciones de redirección no están permitidas en Server Components en Next.js.
-
-      En este caso, la información que proporcionas sugiere que hay un intento de redireccionar a /login desde el servidor. La parte relevante del mensaje de error es:
-
-      rust
-      Copy code
-      digest: 'NEXT_REDIRECT;replace;http://localhost:3000/login?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Flogin;false',
-      Este tipo de redirección debería manejarse en el lado del cliente para evitar problemas en Server Components.
-
-      */
       console.log('nextUrl:', nextUrl, 'isOnMain:', isOnMain, 'isOn:', isOn);
-      if (isOnMain) {
-        if (isLoggedIn) return true; //<--(/main  logeado)
-        return false; //<-- (/main  no logeado) Redirect unauthenticated users to login page
-      }
-      if (isLoggedIn && isOn) {
+      if (isOn) {
+        if (isOnMain) {
+          if (isLoggedIn) return true; //<--(/main  logeado)
+          return false; //<-- (/main  no logeado) Redirect unauthenticated users to login page
+        }
         return true;
       }
     },
